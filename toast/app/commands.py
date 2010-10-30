b405652c9feed5fe02604c652860c12782cbfd25
benbc <ben@bridesmere.com>
2010-10-30 21:43:45 +0100

    Start of command infrastructure.

---
+++ toast/app/commands.py
@@ -0,0 +1,5 @@
+class AddLineCommand:
+    def __init__(self, line):
+        self._line = line
+    def __str__(self):
+        return self._line
--- toast/controllers/command.py
+++ toast/controllers/command.py
@@ -4,11 +4,12 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from toast.lib.base import BaseController, render
+from toast.app.commands import AddLineCommand
 
 log = logging.getLogger(__name__)
 
 class CommandController(BaseController):
     def add_line(self):
-        log.debug(request.params['line'])
-
+        command = AddLineCommand(request.params['line'])
+        log.debug(command)
         redirect(url(controller='view'))
