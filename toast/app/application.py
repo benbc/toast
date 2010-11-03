a52202331f3da8780727325b84cb23a27f75cfc0
benbc <ben@bridesmere.com>
2010-11-03 21:06:07 +0000

    More sketching out of Application.

--- toast/app/application.py
+++ toast/app/application.py
@@ -2,7 +2,7 @@ class Process:
     def start(self):
         import multiprocessing
         (self._pipe_out, self._pipe_in) = multiprocessing.Pipe(False)
-        p = multiprocessing.Process(target=self.init)
+        p = multiprocessing.Process(target=self.run)
         p.start()
 
     def send(self, command):
@@ -11,16 +11,27 @@ class Process:
     def receive(self):
         return self._pipe_out.recv()
 
-    def init(self):
+    def run(self):
+        self.init()
         while True:
             self.loop()
 
+    def init(self):
+        """Override if the process needs initialization."""
+        pass
+
 class Application(Process):
+    def init(self):
+        self.replay()
+
     def loop(self):
         command = self.receive()
         print("In app: %s" % command)
         self.persist(command)
         self.execute(command)
+
+    def replay(self):
+        pass
 
     def persist(self, command):
         pass
