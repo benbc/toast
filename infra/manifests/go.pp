dbe4a3c35a3edbf1fa24c8f119d23b7a84701439
Ben Butler-Cole <ben@bridesmere.com>
2011-09-12 20:30:04 +0100

    Check in Go config.

--- infra/manifests/go.pp
+++ infra/manifests/go.pp
@@ -3,19 +3,19 @@ $downloads = "$share/downloads"
 
 define wget($source) {
   $destination = "$downloads/$name"
-  exec { "/usr/bin/wget --output-document=$destination $source/$name":
+  exec {"/usr/bin/wget --output-document=$destination $source/$name":
     creates => $destination,
   }
 }
 
 define repo() {
-  file { "$name.list":
+  file {"$name.list":
     path => "/etc/apt/sources.list.d/$name.list",
     source => "$share/$name.list",
     notify => Exec['apt-get-update']
   }
 
-  exec { 'ensure-apt-get-update-is-called-before-the-repo-is-used':
+  exec {'ensure-apt-get-update-is-called-before-$name-is-used':
     command => '/bin/true',
     subscribe => File["$name.list"],
     require => Exec['apt-get-update'],
@@ -23,35 +23,41 @@ define repo() {
   }
 }
 
-exec { 'apt-get-update':
+exec {'apt-get-update':
   command => '/usr/bin/apt-get update',
   refreshonly => true,
 }
 
-group { "puppet":
+group {"puppet":
   ensure => "present",
 }
 
-$go_package = 'go-server-2.3.0-14056.deb'
-wget { $go_package:
-  source => 'http://download01.thoughtworks.com/go/2.3/ga',
-}
+repo {'lucid-partner': }
 
-package {'unzip':
+package {'git-core':
   ensure => installed,
-}
-
-package { 'go-server':
-  ensure => installed,
-  provider => dpkg,
-  source => "$downloads/$go_package",
-  require => [Wget[$go_package], Package['sun-java6-jdk'], Package['unzip']],
 }
 
 package {'sun-java6-jdk':
   ensure => installed,
   responsefile => "$share/sun-java-licence.seeds",
   require => Repo['lucid-partner'],
+}
+
+package {'unzip':
+  ensure => installed,
+}
+
+$go_package = 'go-server-2.3.0-14056.deb'
+wget {$go_package:
+  source => 'http://download01.thoughtworks.com/go/2.3/ga',
+}
+
+package {'go-server':
+  ensure => installed,
+  provider => dpkg,
+  source => "$downloads/$go_package",
+  require => [Wget[$go_package], Package['sun-java6-jdk'], Package['unzip']],
 }
 
 service {'go-server':
@@ -62,8 +68,12 @@ service {'go-server':
   require => Package['go-server'],
 }
 
-repo {'lucid-partner': }
-
-package {'git-core':
-  ensure => installed,
+file {'go-config':
+  ensure => link,
+  path => '/etc/go/cruise-config.xml',
+  target => '/tmp/vagrant-share/cruise-config.xml',
+  owner => go,
+  group => go,
+  notify => Service['go-server'],
+  require => Package['go-server'],
 }
