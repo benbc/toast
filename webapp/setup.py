e4459279fec315b8809c63a45af08b2f16e8c40c
Ben Butler-Cole <ben@bridesmere.com>
2012-01-10 20:44:11 +0000

    Remove unnecessary stuff generated by Pylons.

--- webapp/setup.py
+++ webapp/setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
@@ -15,17 +9,12 @@ requires = [
 setup(name='toast',
       version='0.0',
       description='toast',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+      long_description='toast',
+      classifiers=[],
       author='',
       author_email='',
       url='',
-      keywords='web pyramid pylons',
+      keywords='',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
@@ -37,4 +26,3 @@ setup(name='toast',
       main = toast:main
       """,
       )
-
--- webapp/toast/__init__.py
+++ webapp/toast/__init__.py
@@ -1,10 +1,8 @@
 from pyramid.config import Configurator
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'static')
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
