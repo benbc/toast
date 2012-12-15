83ed693f77a50a8f04dcadf17fe342e124be522a
Ben Butler-Cole <ben@bridesmere.com>
2012-12-15 21:11:45 +0000

    Compojure skeleton project.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject toast "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.3"]]
+  :plugins [[lein-ring "0.7.5"]]
+  :ring {:handler toast.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
---
+++ test/toast/test/handler.clj
@@ -0,0 +1,14 @@
+(ns toast.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        toast.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
