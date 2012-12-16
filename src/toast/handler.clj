2fbfffb8f51973bcf747b2c088ed0de3015a09af
Ben Butler-Cole <ben@bridesmere.com>
2012-12-16 22:38:24 +0000

    Support searching for recipes.

--- src/toast/handler.clj
+++ src/toast/handler.clj
@@ -1,6 +1,6 @@
 (ns toast.handler
   (:use compojure.core
-        [clojure.string :only [split-lines]]
+        [clojure.string :only [split split-lines]]
         [ring.util.response :only [redirect]])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
@@ -25,6 +25,21 @@
 
 (defn book-titles []
   (.list (io/file book-root)))
+
+(defn path-elems [path] (split path #"/"))
+(defn files-below [root] (rest (file-seq (io/file root))))
+
+(defn all-recipes []
+  (letfn [(extract-info [file] (drop 3 (path-elems (.getPath file))))
+          (recipe? [path] (= 2 (count path)))]
+    (->> (files-below book-root)
+         (map extract-info)
+         (filter recipe?)
+         (map #(array-map :book (first %) :recipe (second %))))))
+
+(defn search-recipes [substring]
+  (letfn [(name-matches [recipe] (.contains (:recipe recipe) substring))]
+    (filter name-matches (all-recipes))))
 
 (defn valid-name [name]
   (re-find #"^[A-Za-z0-9- ]+$" name))
