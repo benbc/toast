c69baa378045b2f60c474c0913b71dda40947014
Ben Butler-Cole <ben@bridesmere.com>
2012-12-16 20:08:20 +0000

    Add recipes. Filesystem storage.

--- src/toast/handler.clj
+++ src/toast/handler.clj
@@ -1,15 +1,31 @@
 (ns toast.handler
   (:use compojure.core
-        [net.cgrand.enlive-html :only [html-resource emit*]])
+        [net.cgrand.enlive-html :only [html-resource emit*]]
+        [clojure.string :only [split-lines]]
+        [ring.util.response :only [redirect]])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.java.io :as io]))
 
 (def index (html-resource "toast/index.html"))
 (def add (html-resource "toast/add.html"))
 
+(def book-root "./var/books")
+(defn book-path [title] [book-root title])
+(defn recipe-path [book recipe] (conj (book-path book) recipe))
+
+(defn add-recipes [title recipes]
+  (doseq [recipe recipes]
+    (let [recipe-file (apply io/file (recipe-path title recipe))]
+      (io/make-parents recipe-file)
+      (spit recipe-file ""))))
+
 (defroutes app-routes
   (GET "/" [] (emit* index))
   (GET "/add" [] (emit* add))
+  (POST "/add" [title recipes]
+        (add-recipes title (split-lines recipes))
+        (redirect "/"))
   (route/not-found "Not Found"))
 
 (def app
